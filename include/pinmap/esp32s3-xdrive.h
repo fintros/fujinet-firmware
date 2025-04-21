19f560e101b7451657614f21f21882071c8e9015
Alexey Spirkov <dev@alsp.net>
2025-04-22 00:11:14 +0300

    boot complete

--- include/pinmap/esp32s3-xdrive.h
+++ include/pinmap/esp32s3-xdrive.h
@@ -1,5 +1,7 @@
 #ifdef PINMAP_ESP32S3_XDRIVE
 
+#define IF_ENABLE_PIN           GPIO_NUM_5
+
 /* SD Card */
 #define PIN_CARD_DETECT         GPIO_NUM_NC // GPIO_NUM_48 fnSystem.h
 #define PIN_CARD_DETECT_FIX     GPIO_NUM_NC // fnSystem.h
@@ -10,12 +12,12 @@
 #define PIN_SD_HOST_SCK         GPIO_NUM_17
 
 /* UART */
-#define PIN_UART0_RX            GPIO_NUM_9  // fnUART.cpp
-#define PIN_UART0_TX            GPIO_NUM_8
+#define PIN_UART0_RX            GPIO_NUM_44  // fnUART.cpp
+#define PIN_UART0_TX            GPIO_NUM_43
 #define PIN_UART1_RX            GPIO_NUM_NC
 #define PIN_UART1_TX            GPIO_NUM_NC
-#define PIN_UART2_RX            GPIO_NUM_NC
-#define PIN_UART2_TX            GPIO_NUM_NC
+#define PIN_UART2_RX            GPIO_NUM_9
+#define PIN_UART2_TX            GPIO_NUM_8
 
 /* Buttons */
 #define PIN_BUTTON_A            GPIO_NUM_NC
@@ -33,8 +35,8 @@
 #define PIN_PROC                GPIO_NUM_NC //GPIO_NUM_12
 #define PIN_CKO                 GPIO_NUM_NC //GPIO_NUM_6
 #define PIN_CKI                 GPIO_NUM_NC //GPIO_NUM_7
-#define PIN_MTR                 GPIO_NUM_11
-#define PIN_CMD                 GPIO_NUM_19
+#define PIN_MTR                 GPIO_NUM_NC //GPIO_NUM_11
+#define PIN_CMD                 GPIO_NUM_10
 
 
 /* Audio Output */
